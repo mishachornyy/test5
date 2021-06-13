d719e25a10beb4412c1df3f37f3e4649f03e3898
mishachornyy <85844519+mishachornyy@users.noreply.github.com>
2021-06-14 00:56:22 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+const swiper1 = new Swiper(".mySwiper1", {
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+
+    breakpoints: {
+        767: {
+            slidesPerView: 1,
+            allowTouchMove: true,
+        },
+        1108: {
+            allowTouchMove: true,
+            spaceBetween: 10,
+            slidesPerView: 3,
+        },
+        1499: {
+            allowTouchMove: true,
+            spaceBetween: 50,
+            slidesPerView: 3,
+        },
+    },
+});
